4ce326e3ff392d141aa41f7f301714c05a6a01f0
Okamoto5976 <kmm2559327@stu.o-hara.ac.jp>
2026-07-15 00:52:20 +0900

    merge : GachaSystem

---
+++ GachaGameUnity/Assets/GachaSystem/GachaSysteme.cs
@@ -0,0 +1,180 @@
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+[System.Serializable]
+public class Rate
+{
+    public float rateC;
+    public float rateU;
+    public float rateR;
+}
+
+public class GachaSysteme : MonoBehaviour
+{
+    //[SerializeField] private int m_RollNum = 0;
+    public List<int> m_RarityList = new();
+    [SerializeField] public Rate rate;
+
+    [SerializeField] private int m_NeedMoney;
+    [SerializeField] private int m_NeedTicket;
+    //SO
+    [SerializeField] private int m_Coine;
+    [SerializeField]private float m_Ticket;
+
+    public void OneGacha()
+    {
+
+
+        if (m_NeedTicket <= m_Ticket)
+        {
+            if (!IsCheakTicket(m_NeedTicket))
+            {
+                Debug.Log("you do not have ticket");
+                return;
+            }
+            GachaGet(1);
+        }
+        else if (m_NeedMoney <= m_Coine)
+        {
+            if (!IsCheakMoney(m_NeedMoney))
+            {
+                Debug.Log("you do not have money");
+                return;
+            }
+            GachaGet(1);
+        }
+        else
+        {
+            Debug.Log("you do not have to ticket and Coine");
+        }
+            
+    }
+
+    public void TenGacha()
+    {
+        //Do you have Money?
+        //LotteryTypeTen();
+        if (m_NeedTicket * 10 <= m_Ticket)
+        {
+            if (!IsCheakTicket(m_NeedTicket * 10))
+            {
+                Debug.Log("you do not have ticket");
+                return;
+            }
+            GachaGet(10);
+        }
+        else if (m_NeedMoney * 10 <= m_Coine)
+        {
+            if (!IsCheakMoney(m_NeedMoney * 10))
+            {
+                Debug.Log("you do not have money");
+                return;
+            }
+            GachaGet(10);
+        }
+        else
+        {
+            Debug.Log("you do not have to ticket and Coine");
+        }
+
+
+    }
+
+
+
+    private void GachaGet(int num)
+    {
+        m_RarityList.Clear();
+
+        for(int i  = 0; i < num; i++)
+        {
+            float randomPoint = Random.Range(0, 100);
+            int rarity = 0;
+
+
+            if(randomPoint >rate.rateR )
+            {
+                rarity = 3;
+            }
+            else if( randomPoint>rate.rateU )
+            {
+                rarity = 2;
+            }
+            else
+            {
+                rarity = 1;
+            }
+
+            Debug.Log($"{rarity}");
+
+            m_RarityList.Add(rarity);
+
+        }
+
+        //Post PHP data "m_RarityList";
+
+    }
+
+    public void ChiketsGet()
+    {
+        m_Ticket += 1;
+
+    }
+
+    private bool IsCheakMoney(int money)
+    {
+        if(m_Coine < money)
+        {
+            return false;
+        }
+        else
+        {
+            m_Coine -= money;
+            return true;
+        }
+    }
+
+    private bool IsCheakTicket(int ticket)
+    {
+        if (m_Ticket < ticket)
+        {
+            return false;
+        }
+        else
+        {
+            m_Ticket -= ticket;
+            return true;
+        }
+    }
+
+
+    //public void PayTicket(int pay)
+    //{
+    //    m_Ticket -= pay;
+    //    return;
+    //}
+
+    //public void GetCharaSorting(int Index)
+    //{
+    //    CharaData chara = m_DropChara[Index];
+    //    already Charactur check
+    //     Compare the characters you have here with the ones you got from the gacha.
+    //        if (m_DropChara.Exists(name == chara.m_name))
+    //    {
+
+    //        //    //Debug.Log($"Already acquired character: {[(int)Character.Name, charaId]}");
+    //        ChiketsGet();
+    //        //    Debug.Log("It has been converted into a gacha ticket!" );
+    //    }
+    //    else
+    //    {
+    //        m_DropChara.Add(chara);
+
+    //        //Debug.Log($"newCharactur {}‚ª’Ç‰Á‚³‚ê‚Ü‚µ‚½B");
+    //    }
+
+    //}
+}
+
---
+++ GachaGameUnity/Assets/GachaSystem/GatchaButton.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GachaBottun : MonoBehaviour
+{
+    [SerializeField]
+    private GachaSysteme gacha;
+    public void OnClickOne()
+    {
+        //gacha.LotteryTypeOne();  
+    }
+   
+    public void OnClickTen()
+    {
+        //gacha.LotteryTypeTen();
+    }
+}
